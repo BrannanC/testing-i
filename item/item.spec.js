54612936070fe13ac564201af9fc78d95974ef6f
BrannanC <brannanconrad@gmail.com>
2019-03-04 15:53:49 -0600

    Finished tests

--- item/item.spec.js
+++ item/item.spec.js
@@ -7,6 +7,30 @@ const shield = {
     enhancement: 1
 }
 
+const sword = {
+    displayName: '[+6] sword',
+    baseName: 'sword',
+    type: 'weapon',
+    durability: 93,
+    enhancement: 6
+}
+
+const sixShield = {
+    displayName: '[+6] shield',
+    baseName: 'shield',
+    type: 'armor',
+    durability: 93,
+    enhancement: 6
+}
+
+const fifteenShield = {
+    displayName: '[+15] shield',
+    baseName: 'shield',
+    type: 'armor',
+    durability: 93,
+    enhancement: 15
+}
+
 const badShield = {
     displayName: '[+1] shield',
     baseName: 'shield',
@@ -42,7 +66,6 @@ describe('item.js', () => {
         });
     });
 
-
     describe('repair', () => {
         it('should set durability to 100', () => {
             expect(item.repair(shield).durability).toBe(100);
@@ -50,11 +73,37 @@ describe('item.js', () => {
     });
 
     describe('failure', () => {
-        it('should not change durability', () => {
+        it('should not change durability armor < 5', () => {
             expect(item.failure(shield).durability).toBe(shield.durability);
         });
 
-        it('should set durability to 90', () => {
+        it('should not change durability weapon < 7', () => {
+            expect(item.failure(sword).durability).toBe(sword.durability);
+        });
+
+        it('should reduce durability by 5 (enhancement < 15', () => {
+            expect(item.failure(sixShield).durability).toBe(88);
+        });
+
+        it('should reduce durability by 10 (enhancement >= 15', () => {
+            expect(item.failure(fifteenShield).durability).toBe(83);
+        });
+
+        it('should reduce durability below 20 (enhancement <= 14', () => {
+            expect(item.failure({
+                ...sixShield,
+                durability: 23
+            }).durability).toBe(20);
+        });
+
+        it('should reduce durability below 0 (enhancement > 14', () => {
+            expect(item.failure({
+                ...fifteenShield,
+                durability: 8
+            }).durability).toBe(0);
+        });
+
+        it('should reduce durability by 10 and enhancement by 1 (enhancement > 16)', () => {
             expect(item.failure(maxShield)).toEqual({
                 ...maxShield,
                 displayName: '[TET] shield',
